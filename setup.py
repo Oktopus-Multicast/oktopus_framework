b05c80d5cc50971f0c05994b5a6936394fd1e0fd
Carlos Lee <charlee593@gmail.com>
2020-07-01 17:56:18 -0700

    Remove unnecessary dependency.

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # coding=utf-8
+import os
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='oktopus',
@@ -9,6 +13,7 @@ setup(
     author='Khaled Diab, Carlos Lee',
     author_email='kdiab@sfu.ca, carlosl@sfu.ca',
     package_dir={'': 'framework'},
+    install_requires=required,
     packages = find_packages(),
     url='https://github.com/Oktopus-Multicast/oktopus_framework.git',
     classifiers=[
